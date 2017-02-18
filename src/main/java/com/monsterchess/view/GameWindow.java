d41f3382ab20a564dacf0643cbd3a7e5715abd3b
jcbauschatz <jcbauschatz@revolutionehr.com>
2017-02-18 10:08:38 -0600

    More small UI immprovements

--- src/main/java/com/monsterchess/view/GameWindow.java
+++ src/main/java/com/monsterchess/view/GameWindow.java
@@ -25,16 +25,16 @@ public class GameWindow extends JFrame {
 	}
 
 	public GameWindow(GameEngine engine) {
+		super("Monster Chess");
 		setLayout(new BorderLayout());
 
 		boardView = new BoardView(engine);
 		add(boardView, BorderLayout.CENTER);
 
-		GameHistory history = new GameHistory(engine);
-		add(history, BorderLayout.EAST);
+		GameStatusPanel gameStatusPanel = new GameStatusPanel(engine);
+		add(gameStatusPanel, BorderLayout.EAST);
 
 		setSize(new Dimension(1000, 800));
-
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setVisible(true);
