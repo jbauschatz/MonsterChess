d3d3267a20a751932e3cf6ae646ab95c7b7dd46c
jcbauschatz <jcbauschatz@revolutionehr.com>
2017-02-18 00:31:59 -0600

    Small UI improvements

--- src/main/java/com/monsterchess/view/Main.java
+++ src/main/java/com/monsterchess/view/Main.java
@@ -5,12 +5,23 @@ import com.monsterchess.model.GameEngine;
 import com.monsterchess.player.GreedyPlayer;
 import com.monsterchess.player.MinMaxPlayer;
 
+import javax.swing.*;
+
 /**
  *
  */
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (ClassNotFoundException
+				| InstantiationException
+				| UnsupportedLookAndFeelException
+				| IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
 		GameEngine engine = new GameEngine();
 
 		GameWindow window = new GameWindow(engine);
